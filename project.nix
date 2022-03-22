bfb46a1f762002f07f9d81d93dacd544c6890623
hugosenari <hugosenari@gmail.com>
2022-03-22 00:42:54 -0300

    feat(nimpkgs): direct ref

--- project.nix
+++ project.nix
@@ -13,13 +13,18 @@ let
       refs."${lowerName}-unstable" or
       refs."${lowerName}-develop"  or
       (trace refNames refs.${firstRef});
+    baseOutput = { inherit meta; };
+    output = {
+      defaultPackage.${system} = mainPkg.defaultPackage.${system};
+      packages.${system} = mapAttrs (k: v: v.defaultPackage.${system}) refs;
+      dependencies.${system} = mapAttrs (k: v: v.dependencies) refs;
+    };
   in 
-  {
-    inherit meta;
-    defaultPackage.${system} = mainPkg.defaultPackage.${system};
-    packages.${system} = mapAttrs (k: v: v.defaultPackage.${system}) refs;
-    dependencies.${system} = mapAttrs (k: v: v.dependencies) refs;
-  };
+    if builtins.length refNames > 0
+    then baseOutput // output
+    else trace 
+      "No branch or tags for project ${lowerName}" 
+      baseOutput;
 in
 {
   inherit mkProjectOutputFor;
