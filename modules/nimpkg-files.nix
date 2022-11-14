2cb8f99b54edbd2d7e171c4e3b432b59a8907d6a
hugosenari <hugosenari@gmail.com>
2022-11-14 11:15:54 -0300

    feat: wip module 2 nimpkg

--- modules/nimpkg-files.nix
+++ modules/nimpkg-files.nix
@@ -11,30 +11,23 @@ let
     if opts.nim == null 
     then "${pkgs.nim-unwrapped}/bin/nim" 
     else "${opts.nim}/bin/nim";
-  cLibs   = name: opts: 
-    if      opts.cLibs == null
+  runDeps = name: opts: 
+    if      opts.runDeps == null
     then "" 
-    else if builtins.typeOf opts.cLibs == "list" 
-    then builtins.concatStringsSep ":" (map (v: "::${v}") opts.cLibs)
-    else "${builtins.typeOf opts.cLibs}";
-  runLibs = name: opts: 
-    if      opts.cLibs == null
-    then "" 
-    else if builtins.typeOf opts.runLibs == "list" 
-    then builtins.concatStringsSep ":" (map (v: "::${v}") opts.runLibs)
-    else "${builtins.typeOf opts.runLibs}";
+    else if builtins.typeOf opts.runDeps == "list" 
+    then builtins.concatStringsSep ":" (map (v: "::${v}") opts.runDeps)
+    else "${builtins.typeOf opts.runDeps}";
   deps = name: opts: 
     if      opts.deps == null
     then "" 
     else if builtins.typeOf opts.deps == "list" 
     then builtins.concatStringsSep ":" (map (v: "::${v}") opts.deps)
-    else "${builtins.typeOf opts.runLibs}";
+    else "${builtins.typeOf opts.deps}";
   pkg     = name: opts: ''
     cd $PRJ_ROOT
     # name ${name}
-    # clibs ${cLibs name opts}
-    # runLibs ${runLibs name opts}
     # deps ${deps name opts}
+    # runDeps ${runDeps name opts}
     ${cmd name opts} ${opts.cmd} ${toString opts.file}
 
   '';
